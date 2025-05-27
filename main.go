a1e849a1d3ed210cd80fbddff37ea28dd0ec73df
Iliailysha <baryshnikov.i2011@gmail.com>
2025-05-27 19:40:37 +0300

    1st HTTP server and README

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hellow world")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Жопа привет!")
+	})
+	log.Println("Server is running on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
